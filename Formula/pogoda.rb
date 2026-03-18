4559e40cd75d823a7229e2f82bac85f5cd3c396d
Aleksander Kurczyk <aleksander@kurczyk.xyz>
2026-03-18 17:08:11 +0100

    v0.3

--- Formula/pogoda.rb
+++ Formula/pogoda.rb
@@ -1,18 +1,18 @@
 class Pogoda < Formula
   desc "Terminal weather forecast"
   homepage "https://github.com/akurczyk/pogoda"
-  version "0.2"
+  version "0.3"
   license "BSD-2-Clause"
 
   on_macos do
     on_arm do
-      url "https://github.com/akurczyk/pogoda/releases/download/v0.2/pogoda-aarch64-apple-darwin.tar.gz"
-      sha256 "739d24ec3a0cba81b95da3fc09f7561e65cd3786f673224e579b7f53895d22bb"
+      url "https://github.com/akurczyk/pogoda/releases/download/v0.3/pogoda-aarch64-apple-darwin.tar.gz"
+      sha256 "628cb24671127b3fb0072d332206249bfecfa9a88cc925a0cd0daeb090f1850f"
     end
 
     on_intel do
-      url "https://github.com/akurczyk/pogoda/releases/download/v0.2/pogoda-x86_64-apple-darwin.tar.gz"
-      sha256 "649293468b35ad42e855f8a4bf1b6ba360111426281e940d1bbc049e0ea4030a"
+      url "https://github.com/akurczyk/pogoda/releases/download/v0.3/pogoda-x86_64-apple-darwin.tar.gz"
+      sha256 "8ae5e059010ab98bf974d5e2338c70a89d98f35272507b283dfe39c51b933fda"
     end
   end
 
